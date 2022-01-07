791f474315109980ab8702750525c75a5bbecc58
Reed Barger <39719154+reedbarger@users.noreply.github.com>
2022-01-06 22:20:20 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,10 +4,7 @@ export const defaultUser = {
   id: uuid(),
   username: "username",
   name: "name",
-  profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF"
-  // profile_image:
-  // "https://instagram.com/static/images/anonymousUser.jpg/23e7b3b2a737.jpg"
+  profile_image: "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png"
 };
 
 export function getDefaultUser() {
@@ -15,8 +12,7 @@ export function getDefaultUser() {
     id: uuid(),
     username: "username",
     name: "name",
-    profile_image:
-      "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF"
+    profile_image: "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png"
   };
 }
 
@@ -25,8 +21,7 @@ export const defaultPost = {
   likes: 10,
   caption: `<span class="">Do you know the 10 JavaScript concepts you need to learn React? 🤔⚛️👇<br>•<br>•<br>👉 Get the FREE cheatsheet to learn them now: bit.ly/10-js-tips 🔥</span>`,
   user: defaultUser,
-  media:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80126161_2456912044637768_8188145638093587415_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_cat=108&_nc_ohc=GAkCBeSLLnwAX_Xn25U&oh=58af570f5ad051b24583c388b946784a&oe=5EA44484",
+  media: "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/post.jpeg",
   comments: [],
   created_at: "2020-02-28T03:08:14.522421+00:00"
 };
@@ -37,8 +32,7 @@ export function getDefaultPost() {
     likes: 10,
     caption: `<span class="">Do you know the 10 JavaScript concepts you need to learn React? 🤔⚛️👇<br>•<br>•<br>👉 Get the FREE cheatsheet to learn them now: bit.ly/10-js-tips 🔥</span>`,
     user: defaultUser,
-    media:
-      "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80126161_2456912044637768_8188145638093587415_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_cat=108&_nc_ohc=GAkCBeSLLnwAX_Xn25U&oh=58af570f5ad051b24583c388b946784a&oe=5EA44484",
+    media: "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/post.jpeg",
     comments: [],
     created_at: "2020-02-28T03:08:14.522421+00:00"
   };
@@ -64,9 +58,8 @@ export const defaultCurrentUser = {
   id: uuid(),
   username: "me",
   name: "myself",
-  profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
-  website: "https://react12.io",
+  profile_image: "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png",
+  website: "https://reactbootcamp.com",
   email: "me@gmail.com",
   bio: "This is my bio",
   phone_number: "555-555-5555",
